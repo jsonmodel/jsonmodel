e94009ad8f06e1d2706a83c7f841130a1abb6c31
Marin Todorov <marin@underplot.com>
2012-12-18 11:39:33 +0100

    added an umbrella header

---
+++ JSONModel/JSONModelLib.h
@@ -0,0 +1,23 @@
+//
+//  JSONModelLib.h
+//  JSONModelDemo
+//
+//  Created by Marin Todorov on 18/12/2012.
+//  Copyright (c) 2012 Underplot ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//basic JSONModel classes
+#import "JSONModelError.h"
+#import "JSONValueTransformer.h"
+#import "JSONModel.h"
+
+//network classes
+#import "JSONHTTPClient.h"
+#import "JSONModel+networking.h"
+#import "JSONAPI.h"
+
+//models array
+#import "JSONModelArray.h"
+
