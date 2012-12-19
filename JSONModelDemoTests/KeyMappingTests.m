c1a15807bca843949141f2029acc1f9fcca009c0
Marin Todorov <marin@underplot.com>
2012-12-19 23:59:35 +0100

    unit test for key mapping

--- JSONModelDemoTests/KeyMappingTests.m
+++ JSONModelDemoTests/KeyMappingTests.m
@@ -7,7 +7,40 @@
 //
 
 #import "KeyMappingTests.h"
+#import "JSONModelLib.h"
+#import "GitHubKeyMapRepoModel.h"
 
 @implementation KeyMappingTests
+{
+    NSArray* json;
+}
+
+-(void)setUp
+{
+    [super setUp];
+    
+    NSString* filePath = [[NSBundle mainBundle].resourcePath stringByAppendingPathComponent:@"github-iphone.json"];
+    NSData* jsonData = [NSData dataWithContentsOfFile:filePath];
+    
+    NSAssert(jsonData, @"Can't fetch test data file contents.");
+    
+    NSError* err;
+    NSDictionary* jsonDict = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:&err];
+    json = jsonDict[@"repositories"];
+    
+    NSAssert(!err, [err localizedDescription]);
+    
+    NSAssert(jsonData, @"Could not load the test data file.");
+    
+}
+
+-(void)testKeyMapping
+{
+    NSDictionary* repo1 = json[0];
+    GitHubKeyMapRepoModel* model1 = [[GitHubKeyMapRepoModel alloc] initWithDictionary:repo1 error:nil];
+    NSAssert(model1, @"Could not initialize model");
+    
+    NSAssert([model1.__description isEqualToString:repo1[@"description"]], @"__description was not mapped properly");
+}
 
 @end
