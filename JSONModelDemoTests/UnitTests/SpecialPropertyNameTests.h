34363d5b4db80a03a24c92de57cd9a1baafd6e42
BB9z <bb9z@myopera.com>
2013-04-27 00:13:48 +0800

    Add a unit test for special property name.

---
+++ JSONModelDemoTests/UnitTests/SpecialPropertyNameTests.h
@@ -0,0 +1,13 @@
+//
+//  SpeicalPropertyNameTest.h
+//  JSONModelDemo_OSX
+//
+//  Created by BB9z on 13-4-26.
+//  Copyright (c) 2013年 Underplot ltd. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SpecialPropertyNameTests : SenTestCase
+
+@end
