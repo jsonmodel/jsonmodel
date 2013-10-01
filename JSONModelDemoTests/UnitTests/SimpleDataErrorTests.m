123ab3c1f6e70430573c0a4f5293d548da09430c
Jay Abbott <jay@abbott.me.uk>
2013-10-01 15:01:56 +0100

    Clarify test-failure message.

--- JSONModelDemoTests/UnitTests/SimpleDataErrorTests.m
+++ JSONModelDemoTests/UnitTests/SimpleDataErrorTests.m
@@ -105,7 +105,7 @@
 	NSError* err = [self performTestErrorsInNestedModelFile:@"nestedDataWithArrayError.json"];
 
 	// Make sure that the error is at the expected key-path
-	STAssertEqualObjects(err.userInfo[kJSONModelKeyPath], @"images[1]", @"kJSONModelKeyPath does not contain the path of the error.");
+	STAssertEqualObjects(err.userInfo[kJSONModelKeyPath], @"images[1]", @"kJSONModelKeyPath does not contain the expected path of the error.");
 }
 
 -(void)testErrorsInNestedModelsDictionary
@@ -113,7 +113,7 @@
 	NSError* err = [self performTestErrorsInNestedModelFile:@"nestedDataWithDictionaryError.json"];
 
 	// Make sure that the error is at the expected key-path
-	STAssertEqualObjects(err.userInfo[kJSONModelKeyPath], @"imagesObject.image2", @"kJSONModelKeyPath does not contain the path of the error.");
+	STAssertEqualObjects(err.userInfo[kJSONModelKeyPath], @"imagesObject.image2", @"kJSONModelKeyPath does not contain the expected path of the error.");
 }
 
 -(void)testForNilInputFromString
