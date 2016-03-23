d44d7b54d92615416419729aa6cd429917c7ea82
Marin Todorov <marin@underplot.com>
2016-03-23 09:30:29 +0100

    * tests https://github.com/icanzilb/JSONModel/issues/460

---
+++ JSONModelDemoTests/UnitTests/SpecialValuesTests.m
@@ -0,0 +1,50 @@
+
+//
+//  SpecialValuesTests.m
+//  JSONModelDemo_iOS
+//
+//  Created by Marin Todorov on 3/23/16.
+//  Copyright © 2016 Underplot ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "JSONModelLib.h"
+
+//model class
+@interface SpecialModel: JSONModel
+@property (strong, nonatomic) NSString* name;
+@end
+
+@implementation SpecialModel
+@end
+
+//tests class
+@interface SpecialValuesTests : XCTestCase
+@end
+
+@implementation SpecialValuesTests
+{
+    SpecialModel* _model;
+}
+
+- (void)setUp {
+    [super setUp];
+
+    NSString* jsonContents = @"{\"name\": \"FIRST_SECOND\"}";
+    
+    NSError *err;
+    _model = [[SpecialModel alloc] initWithString:jsonContents error:&err];
+    XCTAssertNil(err, "%@", [err localizedDescription]);
+    XCTAssertNotNil(_model, @"Could not load the test data file.");
+}
+
+// tests: https://github.com/icanzilb/JSONModel/issues/460
+- (void)testExample {
+    XCTAssertTrue([_model.name isEqualToString:@"FIRST_SECOND"]);
+}
+
+-(void)tearDown {
+    _model = nil;
+}
+
+@end
