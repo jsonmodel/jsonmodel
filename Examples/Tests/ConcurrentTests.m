f48298110f2cf36057eea31f45b3be4441d373ad
James Billingham <james@jamesbillingham.com>
2016-09-12 10:26:14 +0100

    test cleanup

--- Examples/Tests/ConcurrentTests.m
+++ Examples/Tests/ConcurrentTests.m
@@ -7,53 +7,56 @@
 //
 
 @import JSONModel;
+@import XCTest;
 
-#import <XCTest/XCTest.h>
 #import "ConcurrentReposModel.h"
 
 @interface ConcurrentTests : XCTestCase
-
 @property (nonatomic, strong) id jsonDict;
-
 @end
 
 @implementation ConcurrentTests
 
-- (void)setUp {
+- (void)setUp
+{
 	[super setUp];
-	NSString* filePath = [[NSBundle bundleForClass:[JSONModel class]].resourcePath stringByAppendingPathComponent:@"../../github-iphone.json"];
-	NSData* jsonData = [NSData dataWithContentsOfFile:filePath];
+
+	NSString *filePath = [[NSBundle bundleForClass:[JSONModel class]].resourcePath stringByAppendingPathComponent:@"../../github-iphone.json"];
+	NSData *jsonData = [NSData dataWithContentsOfFile:filePath];
 
 	XCTAssertNotNil(jsonData, @"Can't fetch test data file contents.");
 
-	NSError* err;
+	NSError *err;
 	self.jsonDict = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:&err];
 }
 
-- (void)tearDown {
-	[super tearDown];
-}
-
-- (void)testConcurrentMapping {
+- (void)testConcurrentMapping
+{
 	// Because the uncertainty of concurrency. Need multiple run to confirm the result.
-	NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+	NSOperationQueue *queue = [NSOperationQueue new];
 	queue.maxConcurrentOperationCount = 50;
-	[queue setSuspended:YES];
+	queue.suspended = YES;
 
 	XCTestExpectation *expectation = [self expectationWithDescription:@"Wait for queue...."];
 
 	__block int count = 0;
-	for (int i = 0; i < 100; i++) {
-		[queue addOperationWithBlock:^{
+
+	for (int i = 0; i < 100; i++)
+	{
+		[queue addOperationWithBlock:^
+		{
 			ConcurrentReposModel *model = [[ConcurrentReposModel alloc] initWithDictionary:self.jsonDict error:nil];
 #pragma unused(model)
+
 			count++;
-			if (count == 100) {
+
+			if (count == 100)
 				[expectation fulfill];
-			}
 		}];
 	}
-	[queue setSuspended:NO];
+
+	queue.suspended = NO;
+
 	[self waitForExpectationsWithTimeout:30 handler:nil];
 }
 
